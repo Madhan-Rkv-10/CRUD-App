b6bc2e0a402bb06228fa3d7da127de6f0eac4b31
Madhan-Rkv <madhanrkv10@gmail.com>
2023-07-17 15:06:04 +0530

    Added Entity Class for Employee Table

---
+++ app/src/main/java/com/example/crudapp/EmployeeEntity.kt
@@ -0,0 +1,13 @@
+package com.example.crudapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "employee-table")
+data class EmployeeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "email-id")// internal reference is email-id
+    val email: String = ""// this is for only user Reference
+)
